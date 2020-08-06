21e1bf5d976abd5324d6940322a56e6373763e59
Pablo Dejuan <pablo@pablo.pm>
2020-08-06 15:47:15 +0100

    add log of parms for hello

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,4 +1,6 @@
 exports.handler = function(event, context, callback) {
+  console.log("event", event);
+  console.log("context", context);
   callback(null, {
     statusCode: 200,
     body: "Hello, World"
